9debea6727d8238ce94757ed96bf150b6d71639a
User 01 <user01@teaching-F000.(none)>
2017-03-03 11:59:37 +0000

    day 4 go 2

--- pythonbasics/day4.py
+++ pythonbasics/day4.py
@@ -1,16 +1,36 @@
-import serial
+#!/usr/bin/python2.7
+
+import serial, io
 from datetime import datetime
+
+#ser = serial.Serial(
+#    port='/dev/ttyUSB0',
+#    baudrate=9600,
+#)
+
+#while ser.isOpen():
+#    datastring = ser.read(size=8)
+#    print datetime.utcnow().isoformat(), datastring
+
+#ser.close()
+
+
+outfile='/tmp/serial-temperature.tsv'
 
 ser = serial.Serial(
     port='/dev/ttyUSB0',
     baudrate=9600,
-    bytesize=serial.EIGHTBITS,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE
 )
 
-while ser.isOpen():
-    datastring = ser.read(size=8)
-    print datetime.utcnow().isoformat(), datastring
+sio = io.TextIOWrapper(
+    io.BufferedRWPair(ser, ser, 1),
+    encoding='ascii', newline='\r'
+)
+
+with open(outfile, 'a') as f:
+    while ser.isOpen():
+        datastring = sio.readline()
+        f.write(datetime.utcnow().isoformat() + '\t' + datastring + '\n')
+        f.flush()
 
 ser.close()
